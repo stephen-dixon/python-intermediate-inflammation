9712ab2c30a533793785e97452352c2323431951
sdixon <stephen.dixon@ukaea.uk>
2022-05-25 14:20:52 +0100

    adding serializer

--- inflammation/models.py
+++ inflammation/models.py
@@ -67,3 +67,126 @@ def patient_normalise(data):
     normalised[np.isnan(normalised)] = 0
     normalised[normalised < 0] = 0
     return normalised
+
+
+def attach_names(data, names):
+    """Create datastructure containing patient records."""
+    if len(data) != len(names):
+        raise ValueError("different numbers of names and datasets")
+    named_data = []
+    for name, patient_data in zip(names, data):
+        entry = {"name": name, "data": patient_data}
+        named_data.append(entry)
+    return named_data
+
+
+class Observation:
+    def __init__(self, day, value):
+        self.day = day
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+    def __eq__(self, obj):
+        assert isinstance(obj, Observation)
+        return  obj.day == self.day and obj.value == self.value
+
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+
+class Patient(Person):
+    def __init__(self, name, observations=None):
+        super().__init__(name)
+        self.observations = []
+        if observations is not None:
+            self.observations = observations
+
+    def __eq__(self, obj):
+        assert isinstance(obj, Patient)
+        return self.name == obj.name and self.observations == obj.observations
+
+    def add_observation(self, value, day=None):
+        if day is None:
+            try:
+                day = self.observations[-1]['day'] + 1
+            except IndexError:
+                day = 0
+        new_observation = Observation(day, value)
+        self.observations.append(new_observation)
+        return new_observation
+
+    @property
+    def last_observation(self):
+        return self.observations[-1]
+
+
+class Doctor(Person):
+    def __init__(self, name):
+        super().__init__(name)
+        # list of patient objects? or just a list of keys (names) ?
+        self.patients = []
+
+    # add existing objects or create new ones?
+    def add_patient(self, patient):
+        self.patients.append(patient)
+
+    def average_observations_over_patients(self):
+        means = {}
+        sums = {}
+        ns = {}
+        for patient in self.patients:
+            for obs in patient.observations:
+                if obs.day in sums:
+                    sums[obs.day] += obs.value
+                    ns[obs.day] += 1
+                else:
+                    sums[obs.day] = obs.value
+                    ns[obs.day] = 1
+        for day in sums:
+            means[day] = sums[day] / ns[day]
+        return [Observation(k, v) for k, v in means.items()]
+
+
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+    def __str__(self):
+        return "%s by %s" % (self.title, self.author)
+
+
+class Library:
+    def __init__(self):
+        self.books = []
+
+    def add_book(self, title, author):
+        self.books.append(Book(title, author))
+
+    def __len__(self):
+        return len(self.books)
+
+    def __getitem__(self, key):
+        return self.books[key]
+
+    def by_author(self, author):
+        results = [book for book in self.books if book.author == author]
+        if results == []:
+            raise KeyError("author does not exist in library")
+        return results
+
+    @property
+    def titles(self):
+        return [book.title for book in self.books]
+
+    @property
+    def authors(self):
+        authors = {book.author for book in self.books}
+        return list(authors)
--- tests/test_patient.py
+++ tests/test_patient.py
@@ -1,10 +1,70 @@
 """Tests for the Patient model."""
+from inflammation.models import Patient, Doctor
 
 
 def test_create_patient():
-    from inflammation.models import Patient
-
     name = 'Alice'
     p = Patient(name=name)
-
     assert p.name == name
+
+
+def test_add_patient_obervation():
+    name = 'Alice'
+    p = Patient(name=name)
+    p.add_observation(5)
+    assert len(p.observations) == 1
+    obs = p.observations[0]
+    assert obs.day == 0
+    assert obs.value == 5
+
+
+def test_add_patient_obervation_with_day():
+    name = 'Alice'
+    p = Patient(name=name)
+    days = [0, 1, 2]
+    values = [5, 4, 3]
+    for day, value in zip(days, values):
+        p.add_observation(value, day=day)
+    assert len(p.observations) == len(days)
+    for i, obs in enumerate(p.observations):
+        assert obs.day == days[i]
+        assert obs.value == values[i]
+
+
+def test_create_doctor():
+    name = 'Dr. Alice'
+    d = Doctor(name=name)
+    assert d.name == name
+
+
+def test_add_patients_to_doctor():
+    name = 'Dr. House'
+    d = Doctor(name=name)
+    name = 'Alice'
+    p = Patient(name=name)
+    d.add_patient(p)
+    assert len(d.patients) == 1
+    doctors_patient = d.patients[0]
+    assert doctors_patient.name == p.name
+    assert doctors_patient.observations == p.observations
+
+
+def test_average_observations_over_patients():
+    name = 'Dr. House'
+    d = Doctor(name=name)
+    patient_names = ['alice', 'bob', 'sam']
+    days = [0, 1, 2]
+    values = [5, 4, 3]
+    for name in patient_names:
+        p = Patient(name=name)
+        for day, value in zip(days, values):
+            p.add_observation(value, day=day)
+        d.add_patient(p)
+    avg_obs = d.average_observations_over_patients()
+    assert len(avg_obs) == 3
+    for i, obs in enumerate(avg_obs):
+        assert obs.day == days[i]
+        assert obs.value == values[i]
+
+
+
